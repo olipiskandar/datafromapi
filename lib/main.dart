1a79596c84cc745f840d1c8d930cb3db8521a9ea
Agung Wahyudi <olipiskandar@gmail.com>
2021-02-22 16:54:37 +0700

    initial release

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_get_data/post_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: PostPage(),
+    );
+  }
+}
